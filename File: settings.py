969a30719b16484f50b2759ac844855232c6857a
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-08-13 06:46:22 +0100

    Update and rename README.md to File: settings.py

---
+++ File: settings.py
@@ -0,0 +1,12 @@
+INSTALLED_APPS = [
+    # ... default apps ...
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'crm',
+    'graphene_django',  # graphene-django
+    'django_filters',  # django-filters
+]
