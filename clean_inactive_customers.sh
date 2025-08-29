b334c918b4284355060a3f5a2886001c7f588042
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-08-29 07:32:22 +0100

    Create clean_inactive_customers.sh

---
+++ clean_inactive_customers.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+DELETED_COUNT=$(python manage.py shell -c "
+from django.utils import timezone
+from datetime import timedelta
+from crm.models import Customer
+
+one_year_ago = timezone.now() - timedelta(days=365)
+qs = Customer.objects.filter(orders__isnull=True, created_at__lte=one_year_ago)
+count = qs.count()
+qs.delete()
+print(count)
+")
+
+# Log result with timestamp
+echo \"$(date '+%Y-%m-%d %H:%M:%S') - Deleted $DELETED_COUNT inactive customers\" >> /tmp/customer_cleanup_log.txt
